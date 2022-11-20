f97d17e4f8acc1e86664f637fea1104fc2fd3d0b
yessena-21 <bambr78@yandex.ru>
2022-11-20 23:18:46 +0300

    ПР 13

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
     password: Joi.string().required(),
-    name: Joi.string().min(2),
-    about: Joi.string().min(2),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(linkRegExp),
   }),
 }), createUser);
