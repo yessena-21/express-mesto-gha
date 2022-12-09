ab21ec13526f0023f3e3310e73936be8cde46a54
yessena-21 <bambr78@yandex.ru>
2022-12-09 21:44:47 +0300

    change data

--- app.js
+++ app.js
@@ -24,10 +24,10 @@ const options = {
     'http://yessena.nomoredomains.club',
     'https://yessena.nomoredomains.club',
   ],
-  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-  allowedHeaders: ['Content-Type', 'origin'],
+ // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //preflightContinue: false,
+  //optionsSuccessStatus: 204,
+  //allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
 
