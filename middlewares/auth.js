eac67a21781ca3195acf7d651f6995291dbba793
yessena-21 <bambr78@yandex.ru>
2022-12-12 18:02:38 +0300

    без кук

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,44 @@
+// const jwt = require('jsonwebtoken');
+// const { AuthError } = require('../errors/auth-error');
+
+// module.exports = (req, res, next) => {
+//   // console.log(req.headers);
+// const { authorization } = req.cookie;
+// console.log(authorization);
+// if (!authorization || !authorization.startsWith('Bearer ')) {
+//   return next(new AuthError('Необходима авторизация'));
+// }
+
+// const token = authorization.replace('Bearer ', '');
+
+// const token = req.cookies.jwt;
+// if (!token) {
+//   throw new AuthError('Необходима авторизация');
+// }
+// let payload;
+
+// try {
+//   payload = jwt.verify(token, 'some-secret-key');
+//   req.user = payload; // записываем пейлоуд в объект запроса
+// } catch (err) {
+//   return next(new AuthError('Необходима авторизация'));
+//   // throw new AuthError('Необходима авторизация');
+// }
+
+// return next(); // пропускаем запрос дальше
+
 const jwt = require('jsonwebtoken');
 const { AuthError } = require('../errors/auth-error');
 
 module.exports = (req, res, next) => {
-  // console.log(req.headers);
-  // const { authorization } = req.cookie;
-  // console.log(authorization);
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return next(new AuthError('Необходима авторизация'));
-  // }
+  const { authorization } = req.headers;
 
-  // const token = authorization.replace('Bearer ', '');
-  const token = req.cookies.jwt;
-  if (!token) {
-    throw new AuthError('Необходима авторизация');
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthError('Необходима авторизация'));
   }
+
+  const token = authorization.replace('Bearer ', '');
+
   let payload;
 
   try {
