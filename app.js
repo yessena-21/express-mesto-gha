7118b01b697f1a51daecf5de53f2eed9638cebcf
yessena-21 <bambr78@yandex.ru>
2022-12-15 17:51:09 +0300

    111

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const options = {
   // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   // preflightContinue: false,
   // optionsSuccessStatus: 204,
-  // allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
   credentials: true,
 };
 
